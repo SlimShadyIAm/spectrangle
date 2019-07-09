54333034056100f897e329194ca5b6fd9aec6c48
Aamir Farooq <aamir@farooq.xyz>
2019-07-09 14:21:11 +0500

    SideColor char representation

--- src/Models/SideColor.java
+++ src/Models/SideColor.java
@@ -3,7 +3,7 @@ package Models;
 public enum SideColor {
     R, Y, G, B, P, W;
 
-    public String toString() {
-        return name().charAt(0) + name().substring(1).toLowerCase();
+    public Character getChar() {
+        return (name().charAt(0) + name().substring(1)).charAt(0);
     }
 }
